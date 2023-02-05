4b30e168fc3d8d9c1e24e2e1e7c944099020d508
vera-l <v.lobacheva@hh.ru>
2023-02-05 13:49:00 +0300

    Solve meetup info

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -3,19 +3,50 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      required: true,
+      type: String,
+    },
+    place: {
+      required: true,
+      type: String,
+    },
+    date: {
+      required: true,
+      type: Number,
+    },
+  },
+
+  methods: {
+    formatDateHuman: function(date) {
+      return new Date(date).toLocaleDateString(
+        navigator.language,
+        {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        }
+      );
+    },
+    formatDateDash: function(date) {
+      return new Date(date).toISOString().substring(0, 10);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="formatDateDash(date)">{{ formatDateHuman(date) }}</time>
       </li>
     </ul>`,
 });
