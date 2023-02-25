f27f9450aee8af27a722bd553f08cd4b06a2d196
vera-l <v.lobacheva@hh.ru>
2023-02-25 23:21:15 +0300

    Solve ui input date

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <UiInput />
+  <UiInput :type="type" :step="step" :model-value="value" @input="input">
+    <template v-for="slot in Object.keys($slots)" #[slot]>
+      <slot :name="slot" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
@@ -9,5 +13,63 @@ export default {
   name: 'UiInputDate',
 
   components: { UiInput },
+
+  emits: ['update:modelValue'],
+
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'date',
+      validator (value) {
+        return value === 'date' || value === 'time' || value === 'datetime-local';
+      },
+    },
+    step: {
+      type: String,
+      required: false,
+    },
+    modelValue: {
+      type: [Number, null],
+      required: false,
+      default: null,
+    },
+  },
+
+  computed: {
+    value() {
+      if (!this.modelValue) {
+        return '';
+      }
+      const isoDate = new Date(this.modelValue).toISOString();
+      if (this.type === 'date') {
+        return isoDate.split('T')[0];
+      }
+      if (this.type === 'datetime-local') {
+        return isoDate.split('.')[0];
+      }
+      if (this.type === 'time') {
+        return isoDate.substring(11, 16);
+      }
+      return String(this.modelValue);
+    },
+  },
+
+  methods: {
+    input(e) {
+      let value = e.target.value;
+      if (!value) {
+        value = null;
+      } else {
+        value = e.target.valueAsNumber;
+        if (isNaN(value)) {
+          value = null;
+        }
+      }
+
+      this.$emit('update:modelValue', value)
+    },
+  },
+
 };
 </script>
