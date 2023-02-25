91d332419726f616befcc2e45bcad28c1285b3dc
vera-l <v.lobacheva@hh.ru>
2023-02-25 20:39:14 +0300

    Solve ui image uploader

--- 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
+++ 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="image-uploader">
-    <label class="image-uploader__preview image-uploader__preview-loading" style="--bg-url: url('/link.jpeg')">
-      <span class="image-uploader__text">Загрузить изображение</span>
-      <input type="file" accept="image/*" class="image-uploader__input" />
+    <label class="image-uploader__preview"
+      :class="{
+        'image-uploader__preview-loading': state === 'loading',
+      }"
+      :style="selectedImage ? `--bg-url: url('${selectedImage}')` : null"
+    >
+      <span class="image-uploader__text">
+        {{ text }}
+      </span>
+      <input type="file" accept="image/*"
+        class="image-uploader__input"
+        v-bind="$attrs" ref="input"
+        @click="click"
+        @change="change" />
     </label>
   </div>
 </template>
@@ -10,6 +21,84 @@
 <script>
 export default {
   name: 'UiImageUploader',
+
+  inheritAttrs: false,
+
+  data() {
+    return {
+      state: this.preview ? 'full' : 'empty', // 'empty' | 'loading' | 'full'
+      selectedImage: this.preview ? this.preview : null,
+    };
+  },
+
+  emits: ['upload', 'error', 'remove', 'select'],
+
+  computed: {
+    text() {
+      switch (this.state) {
+        case 'loading':
+          return 'Загрузка...';
+        case 'full':
+          return 'Удалить изображение';
+        case 'empty':
+        default:
+          return 'Загрузить изображение';
+      };
+    },
+  },
+
+  props: {
+    preview: {
+      type: String,
+      required: false,
+    },
+    uploader: {
+      type: Function,
+      required: false,
+    },
+  },
+
+  methods: {
+    click(e) {
+      switch (this.state) {
+        case 'loading':
+          e.preventDefault();
+          break;
+        case 'full':
+          e.preventDefault();
+          this.delete();
+          break;
+      };
+    },
+    delete() {
+      this.state = 'empty';
+      this.selectedImage = null;
+      this.$emit('remove');
+      this.$refs.input.value = null;
+    },
+    async change(e) {
+      const file = this.$refs.input.files[0];
+      this.selectedImage = URL.createObjectURL(file);
+      this.$emit('select', file);
+
+      if (this.uploader) {
+        try {
+          this.state = 'loading';
+          const result = await this.uploader(file);
+          this.$emit('upload', result);
+          this.state = 'full';
+        } catch (error) {
+          this.$emit('error', error);
+          this.state = 'empty';
+          this.selectedImage = null;
+          this.$refs.input.value = null;
+        }
+      } else {
+        this.state = 'full';
+      }
+    },
+  },
+
 };
 </script>
 
