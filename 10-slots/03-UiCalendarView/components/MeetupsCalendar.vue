3fe18ac1457467e4d5b82318aa82e75701d84f5d
vera-l <v.lobacheva@hh.ru>
2023-03-20 00:15:56 +0300

    Solve ui calendar view

--- 10-slots/03-UiCalendarView/components/MeetupsCalendar.vue
+++ 10-slots/03-UiCalendarView/components/MeetupsCalendar.vue
@@ -1,7 +1,7 @@
 <template>
-  <UiCalendarView>
-    <UiCalendarEvent v-if="meetups[0]" tag="a" :href="`/meetups/${meetups[0].id}`">
-      {{ meetups[0].title }}
+  <UiCalendarView v-slot="{ shortDate }">
+    <UiCalendarEvent v-for="meetup in dateByMeetups.get(shortDate)" :key="meetup.id" tag="a" :href="`/meetups/${meetup.id}`">
+      {{ meetup.title }}
     </UiCalendarEvent>
   </UiCalendarView>
 </template>
@@ -24,6 +24,31 @@ export default {
       required: true,
     },
   },
+
+  computed: {
+    dateByMeetups() {
+      const map = new Map();
+      this.meetups.forEach(item => {
+        const dateKey = this.shortDateFormat(new Date(item.date));
+        const dayMeetups = map.get(dateKey);
+        if (!dayMeetups) {
+          map.set(dateKey, [item]);
+        } else {
+          dayMeetups.push(item);
+        }
+      });
+      return map;
+    },
+  },
+
+  methods: {
+    shortDateFormat(date) {
+      const day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+      const month = (date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1);
+      return `${day}-${month}-${date.getFullYear()}`;
+    },
+  },
+
 };
 </script>
 
